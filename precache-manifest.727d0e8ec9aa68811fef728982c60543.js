491f885fe2ed455b83e7664bf6ffa94504f54ee0
Joel Gaeta <joelsgaeta@gmail.com>
2020-01-06 20:32:16 -0500

    Updates

---
+++ precache-manifest.727d0e8ec9aa68811fef728982c60543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfafa2916d95410f20ad9ed01d304bb",
+    "url": "/react-todo-hooks/index.html"
+  },
+  {
+    "revision": "b3ad5673a99cdee25093",
+    "url": "/react-todo-hooks/static/css/main.4018f348.chunk.css"
+  },
+  {
+    "revision": "4118dd12936beeaa46e9",
+    "url": "/react-todo-hooks/static/js/2.690ba5d0.chunk.js"
+  },
+  {
+    "revision": "b3ad5673a99cdee25093",
+    "url": "/react-todo-hooks/static/js/main.73390dfc.chunk.js"
+  },
+  {
+    "revision": "d5e603aa37f1c43f93e5",
+    "url": "/react-todo-hooks/static/js/runtime-main.c6984379.js"
+  }
+]);
